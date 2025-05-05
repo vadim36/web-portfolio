4371b1f783d0032ba41b0e96fbd2e55ec9550110
Vadim Medvedev <vadim130809@gmail.com>
2025-05-05 19:13:01 +0300

    feat: add save theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { ContactSection } from "@/components/ContactSection";
 import { HeroSection } from "@/components/HeroSection";
 import { ProjectsSection } from "@/components/ProjectsSection";
-import { useState } from "react";
 import { Settings } from "./components/Settings";
-import { ThemeContext } from "./shared";
+import { ThemeContext, useLocalStorage } from "./shared";
 
 export function App() {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+  const [isDarkMode, setIsDarkMode] = useLocalStorage<boolean>("isDarkMode", false);
 
   return (
     <ThemeContext.Provider
--- src/shared/hooks/useLocalStorage.ts
+++ src/shared/hooks/useLocalStorage.ts
@@ -1,3 +1,20 @@
-export function useLocalStorage() {
-  
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, value: T): [T, typeof setData] {
+  const [data, setData] = useState<T>(() => {
+    const localData = localStorage.getItem(key);
+
+    if (!localData) {
+      localStorage.setItem(key, JSON.stringify(value));
+      return value;
+    }
+
+    return JSON.parse(localData);
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(data));
+  }, [data, key]);
+
+  return [data, setData];
 }
